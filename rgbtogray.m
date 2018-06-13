8b588d0eaf48582d8507466f5c4a6084889bebde
Jubair Obhi <jubairku12@gmail.com>
2018-06-13 21:23:27 +0600

    Converted grayscale image to binary image

--- rgbtogray.m
+++ rgbtogray.m
@@ -8,8 +8,20 @@ green = double(image(:,:,2));
 blue = double(image(:,:,3));
 %Took average of red green and blue values of pixels
 average = double((red+green+blue)/3);
-%Converted double matrix into integer matrix
+%Converting double matrix into integer matrix
 grayscale = uint8(average);
+%Converting grayscale image to binary image
+binary = grayscale;
+for i=1:size(grayscale,1)
+    for j=1:size(grayscale,2)
+        if(grayscale(i,j))>127
+            binary(i,j)=1;
+        else
+            binary(i,j)=0;
+        end
+    end
+end
+binary=single(binary);
 %Here I will display the image in UI
-figure, imshow(grayscale);
+figure, imshow(binary);
 
